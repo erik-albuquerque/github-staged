182b822127c31a34d99b22e53a465f46b68da3bc
Érik Albuquerque <erik.albuquerque.oficial@gmail.com>
2022-10-17 14:17:03 -0300

    feat: add main page

---
+++ src/constants/rooms.ts
@@ -0,0 +1,8 @@
+const rooms = [
+  { id: "1a2", name: "master", userId: 'userMaster' },
+  { id: "2ds", name: "room01", userId: 'user01' },
+  { id: "req", name: "room02", userId: 'user02' },
+  { id: "pqw", name: "room03", userId: 'user03' },
+];
+
+export { rooms };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,54 @@
 import type { NextPage } from "next";
 import Head from "next/head";
+import { rooms } from "../constants";
 
 const Home: NextPage = () => {
   return (
-    <div>
+    <div
+      style={{
+        width: "100%",
+        height: "100%",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
       <Head>
         <title>Sushi Stage</title>
       </Head>
       <h1>Sushi Stage</h1>
-      
+
+      <h2>Rooms</h2>
+
+      <div
+        style={{
+          maxWidth: "400px",
+          display: "flex",
+          flexDirection: "row",
+          flexWrap: "wrap",
+          gap: "16px",
+        }}
+      >
+        {rooms.map((room) => (
+          <div
+            key={room.id}
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              gap: "8px",
+            }}
+          >
+            <button>{room.name}</button>
+            <span>
+              by{" "}
+              <a href="" style={{ textDecoration: "none" }}>
+                {room.userId}
+              </a>
+            </span>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
